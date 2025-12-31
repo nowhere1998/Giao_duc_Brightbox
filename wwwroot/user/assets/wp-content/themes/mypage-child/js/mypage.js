2f425697d6583947479187d44f60bea036d79b97
nowhere1998 <dinhthien6270790@gmail.com>
2025-12-31 09:05:53 +0700

    update 31/12

---
+++ wwwroot/user/assets/wp-content/themes/mypage-child/js/mypage.js
@@ -0,0 +1,31 @@
+﻿jQuery(document).ready(function ($) {
+	$('.select2_dropdown[name="khoa-hoc"]').select2({
+		placeholder: 'Chá»n khĂ³a há»c*',
+		minimumResultsForSearch: -1,
+		width: 'style'
+	});
+});
+jQuery(document).ready(function ($) {
+	$('.select2_dropdown.monday').select2({
+		placeholder: 'Chá»n mĂ´n giáº£ng dáº¡y*',
+		minimumResultsForSearch: -1,
+		width: 'style'
+	});
+});
+jQuery(document).ready(function ($) {
+	$('.select2_dropdown.vi-tri').select2({
+		placeholder: 'Chá»n vá»‹ trĂ­ á»©ng tuyá»ƒn *',
+		minimumResultsForSearch: -1,
+		width: 'style'
+	});
+});
+document.addEventListener('wpcf7invalid', function (event) {
+	setTimeout(function () {
+		jQuery(".wpcf7 form .wpcf7-response-output").slideDown(300);
+		jQuery(".wpcf7-not-valid-tip").slideDown(300);
+	}, 2000);
+	setTimeout(function () {
+		jQuery(".wpcf7 form .wpcf7-response-output").delay(1000).slideUp(300);
+		jQuery(".wpcf7-not-valid-tip").delay(1000).slideUp(300);
+	}, 3000);
+}, false);
---
+++ wwwroot/user/assets/xmlrpc.php
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://tech1.mypagedemo.com/brightbox</homePageLink>
+
+		<apis>
+			<api
+				name="WordPress"
+				blogID="1"
+				preferred="true"
+				apiLink="https://tech1.mypagedemo.com/brightbox/xmlrpc.php" />
+
+			<api
+				name="Movable Type"
+				blogID="1"
+				preferred="false"
+				apiLink="https://tech1.mypagedemo.com/brightbox/xmlrpc.php" />
+
+			<api
+				name="MetaWeblog"
+				blogID="1"
+				preferred="false"
+				apiLink="https://tech1.mypagedemo.com/brightbox/xmlrpc.php" />
+
+			<api
+				name="Blogger"
+				blogID="1"
+				preferred="false"
+				apiLink="https://tech1.mypagedemo.com/brightbox/xmlrpc.php" />
+
+			<api
+				name="WP-API"
+				blogID="1"
+				preferred="false"
+				apiLink="https://tech1.mypagedemo.com/brightbox/wp-json/" />
+		</apis>
+	</service>
+</rsd>
