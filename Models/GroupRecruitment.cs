15df592654178fe27b5f9640016ebbc3a3dceab4
FIRST_NAME LAST_NAME <lucvanthai16@gmail.com>
2026-02-10 11:07:57 +0700

    update

---
+++ Models/GroupRecruitment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Models
+{
+    public class GroupRecruitment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(256)]
+        public string? Name { get; set; }
+
+        [MaxLength(256)]
+        public string? Tag { get; set; }
+
+        public int Status { get; set; }  // 1: hiển thị, 0: ẩn
+
+        // Navigation property
+        public ICollection<Recruitment>? Recruitments { get; set; } = new List<Recruitment>();
+    }
+}
---
+++ Models/Recruitment.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyShop.Models
+{
+    public class Recruitment
+    {
+        [Key]
+        public int RecruitmentId { get; set; }
+
+        [MaxLength(255)]
+        public string? JobTitle { get; set; }
+
+        public string? JobDescription { get; set; }
+
+        [MaxLength(255)]
+        public string? CompanyName { get; set; }
+
+        [MaxLength(255)]
+        public string? CompanyAddress { get; set; }
+
+        [MaxLength(50)]
+        public string? Phone { get; set; }
+
+        [MaxLength(100)]
+        public string? Email { get; set; }
+
+        [MaxLength(100)]
+        public string? Salary { get; set; }
+
+        public int? GroupRecruitmentId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Status { get; set; }
+
+        // Navigation property
+        [ForeignKey("GroupRecruitmentId")]
+        public GroupRecruitment? GroupRecruitment { get; set; }
+    }
+}
