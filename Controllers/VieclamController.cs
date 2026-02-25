54eb0e3d88295ebe7b990c277fd9de936908bc8a
nowhere1998 <dinhthien6270790@gmail.com>
2026-02-25 16:09:10 +0700

    update

--- Controllers/VieclamController.cs
+++ Controllers/VieclamController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
 
 namespace MyShop.Controllers
 {
 	public class VieclamController : Controller
 	{
-		[Route("viec-lam")]
+		private readonly DbMyShopContext _context;
+        public VieclamController(DbMyShopContext context)
+        {
+            _context = context;
+        }
+        [Route("viec-lam")]
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		[Route("viec-lam-chi-tiet/{slug}")]
+		public IActionResult Chitiet(string slug = "", string s = "")
+		{
+			var tintuc = _context.Recruitments.FirstOrDefault(x => x.Tag == slug) ?? new News();
+			var news = _context.News
+				.OrderByDescending(x => x.Id)
+				.Where(x => x.Active == 1)
+				.Skip(0)
+				.Take(3)
+				.ToList();
+
+			ViewBag.News = news;
+			ViewBag.Search = s;
+			return View("viec-lam-chi-tiet", tintuc);
 		}
 	}
 }
