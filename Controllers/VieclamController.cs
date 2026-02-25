041f59a2233cdf71f0cbedf4b5c965155b736ada
nowhere1998 <dinhthien6270790@gmail.com>
2026-02-25 14:27:25 +0700

    update

---
+++ Controllers/VieclamController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyShop.Controllers
+{
+	public class VieclamController : Controller
+	{
+		[Route("viec-lam")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
